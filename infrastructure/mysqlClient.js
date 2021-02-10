113ef5aa40329231c55b04c0d15b8b2eac38d0e6
Bere <bere_bere92@hotmail.com>
2021-02-10 18:13:45 +0100

    wip

--- infrastructure/mysqlClient.js
+++ infrastructure/mysqlClient.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const mssql = require('mssql');
+const sql = require('mssql');
 
+//create connection
 const config = {
   user: 'appbono',
   password: 'LeBon.1498',
@@ -16,28 +17,27 @@ const config = {
   }
 }
 
-const connection = mssql.connect(config, (err) => {
+const connection = sql.connect(config, (err) => {
   if(err){
-    console.error('error connecting: ' + err);
-  } else{
-      console.log("SQL connected");
+    console.error('Error connecting: ' + err);
   }
+  console.log("SQL connected");
 });
 
 const run = async() =>{
-  // let pool;
+  let pool;
 
   try {
-      // console.log('Connection Opening..');
-      // pool = await mssql.connect(config);
-      // console.log('Connected');
-      const { recordset } = await connection.query`SELECT * FROM APP_COLABORA;`;
+
+      pool = await sql.connect(config);
+      const { recordset } = await sql.query`SELECT * FROM APP_COLABORA;`;
       console.log(recordset);
+
   } catch (err) {
       console.log(err);
 
   }finally{
-      await connection.close();
+      await pool.close();
       console.log('Connection closed');
   }
 }
