ad57c8f9c016632d596944d0c2337bc288e6bb87
Bere <bere_bere92@hotmail.com>
2021-03-10 12:50:31 +0100

    wip

---
+++ tests_code.vue
@@ -0,0 +1,6 @@
+    methods: {
+        normalize: function (city) {
+            let nCity = city.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f-\s-\.]/g, "");
+            return nCity;
+        }
+    },
