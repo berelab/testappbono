af100583ca0ad52130e6b820a4af0bd8d6def943
Bere <bere_bere92@hotmail.com>
2021-03-12 15:31:06 +0100

    cdmex wip

--- infrastructure/cdmex/corteRepo.js
+++ infrastructure/cdmex/corteRepo.js
@@ -61,7 +61,7 @@ class CorteRepository {
     async entryTimes(){
         let response;
         let pool;
-        const queryString = "SELECT cu.* FROM (SELECT u.CB_CODIGO AS userid, CONVERT(VARCHAR(10), c.AU_FECHA, 101) AS fecha, c.CH_H_AJUS AS entrada, c.CH_H_REAL AS entrada_real, ROW_NUMBER() OVER (PARTITION BY u.CB_CODIGO, c.AU_FECHA ORDER BY c.CH_H_AJUS) AS seqnum FROM APP_CHECADAS c JOIN APP_COLABORA u ON u.CB_CODIGO = c.CB_CODIGO JOIN APP_NIVEL2 n ON u.CB_NIVEL2 = n.TB_CODIGO WHERE c.AU_FECHA BETWEEN (SELECT DATEADD(DAY, 2 - DATEPART(WEEKDAY, GETDATE()), cast( floor( cast( getdate() as float)) as datetime))) AND (SELECT  DATEADD(DAY, 8 -  DATEPART(WEEKDAY, GETDATE()) , cast( floor( cast( getdate() as float)) as datetime)) )  AND  u.CB_NIVEL5 = 'LPZ' AND n.TB_ELEMENT IN ('Corte Variable', 'EM Cortado')) cu WHERE seqnum = 1 "
+        const queryString = "SELECT cu.* FROM (SELECT u.CB_CODIGO AS userid, CONVERT(VARCHAR(10), c.AU_FECHA, 101) AS fecha, c.CH_H_AJUS AS entrada, c.CH_H_REAL AS entrada_real, ROW_NUMBER() OVER (PARTITION BY u.CB_CODIGO, c.AU_FECHA ORDER BY c.CH_H_AJUS) AS seqnum FROM APP_CHECADAS c JOIN APP_COLABORA u ON u.CB_CODIGO = c.CB_CODIGO JOIN APP_NIVEL2 n ON u.CB_NIVEL2 = n.TB_CODIGO WHERE c.AU_FECHA BETWEEN (SELECT DATEADD(DAY, 2 - DATEPART(WEEKDAY, GETDATE()), cast( floor( cast( getdate() as float)) as datetime))) AND (SELECT  DATEADD(DAY, 8 -  DATEPART(WEEKDAY, GETDATE()) , cast( floor( cast( getdate() as float)) as datetime)) )  AND  u.CB_NIVEL5 = 'CMX' AND n.TB_ELEMENT IN ('Corte Variable', 'EM Cortado')) cu WHERE seqnum = 1 "
 
         try {
             pool = await prodPoolPromise
@@ -76,7 +76,7 @@ class CorteRepository {
     async extraData(){
         let response;
         let pool;
-        const queryString = "SELECT TOP(1) u.CB_CODIGO as userid, u.CB_NIVEL5 as planta, n.TB_ELEMENT AS depto, t.[TU_CODIGO] as turno, t.[TU_DIAS] as dias, t.[TU_VACA_HA] as factor FROM APP_TURNO t JOIN APP_COLABORA u ON u.CB_TURNO = t.TU_CODIGO JOIN APP_NIVEL2 n ON u.CB_NIVEL2 = n.TB_CODIGO WHERE u.CB_NIVEL5 = 'LPZ' AND n.TB_ELEMENT IN ('Corte Variable', 'EM Cortado')";
+        const queryString = "SELECT TOP(1) u.CB_CODIGO as userid, u.CB_NIVEL5 as planta, n.TB_ELEMENT AS depto, t.[TU_CODIGO] as turno, t.[TU_DIAS] as dias, t.[TU_VACA_HA] as factor FROM APP_TURNO t JOIN APP_COLABORA u ON u.CB_TURNO = t.TU_CODIGO JOIN APP_NIVEL2 n ON u.CB_NIVEL2 = n.TB_CODIGO WHERE u.CB_NIVEL5 = 'CMX' AND n.TB_ELEMENT IN ('Corte Variable', 'EM Cortado')";
 
         try {
             pool = await prodPoolPromise
