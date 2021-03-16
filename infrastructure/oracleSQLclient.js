64ce2ae0ae1218f2886bca746f5c13944d2eddd5
Erik González <erik.gonzalez.ac@gmail.com>
2021-03-16 15:51:10 -0700

    install oracledb

---
+++ infrastructure/oracleSQLclient.js
@@ -0,0 +1,30 @@
+'use strict'
+let connection;
+var oracledb = require('oracledb');
+
+const config = {
+    user: 'appbono',
+    password: 'Bshb1WIktB',
+    connectString: 'pfanoi'
+}
+
+/*
+(async function() {
+try{
+   connection = await oracledb.getConnection({
+        config
+   });
+   console.log("Successfully connected to Oracle!")
+} catch(err) {
+    console.log("Error: ", err);
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch(err) {
+        console.log("Error when closing the database connection: ", err);
+      }
+    }
+  }
+})()
+*/
