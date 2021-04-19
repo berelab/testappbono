f4ae39cbb8ad582414e234b98b00d01e0449da91
JoseLuisVegaHerrera <jlvh1996@gmail.com>
2021-04-19 15:51:48 -0700

    PROD hmo

--- models/hermosillo/moldeoModel.js
+++ models/hermosillo/moldeoModel.js
@@ -1,9 +1,12 @@
 'use strict'
+import moment from 'moment';
 
 class MoldeoModel {
-    constructor(repository){
+    constructor(repository, produccionRepo){
         this.repository = repository;
+        this.produccionRepo =produccionRepo;
     }
+
 
     async execute() {
         let response;
@@ -13,17 +16,20 @@ class MoldeoModel {
         let amp;
         let valorReal = this._convertMonth();
         let year = this._getYear();
+        let produccion;
+        let day = moment().weekday()
         try {
             response = await this.repository.find();
             teamResponse = await this.repository.findTeam();
             entries = await this.repository.entryTimes();
             extra = await this.repository.extraData();
             amp = await this.repository.indicator(year, valorReal);
+            produccion = await this._produccion(day)
         } catch(error) {
             throw error;
         }
 
-        return this._convertData(response, teamResponse, this._reorderData(entries), extra,amp);
+        return this._convertData(response, teamResponse, this._reorderData(entries), extra,amp,produccion);
     }
 
     async refresh(base, dias_sucios, extra_m3) {
@@ -38,7 +44,7 @@ class MoldeoModel {
         return response;
     }
 
-    _convertData(response, team, entries, extra,amp) {
+    _convertData(response, team, entries, extra,amp,produccion) {
         return {
             message: 'Moldeo',
             city: 'Hermosillo',
@@ -50,15 +56,7 @@ class MoldeoModel {
             amp: amp*100, //convertirlo a porcentaje
             rechazos_internos:0,       
             horas_por_turno: 0,         
-            m3_cortados: {
-                lunes: 853.5,
-                martes: 853.5,
-                miercoles: 0,
-                jueves: 853.5,
-                viernes: 853.5,
-                sabado: 0,
-                domingo:0,
-            },
+            m3_cortados: produccion,
             equipo: team,
             team_asis: entries
         };
@@ -146,6 +144,198 @@ class MoldeoModel {
         let year = dateObj.getFullYear();
         month == 0? year = year-1: year  
         return year
+    }
+
+
+    _convertProd(prod){
+        let len = prod.length;
+        let result=0;
+        if(len>0){
+            for(var i=0; i<len; i++){
+                prod[i][1]=='CORTE' ?result= prod[i][3]: result
+            }
+        }
+
+        return result
+    }
+    
+    async _produccion (day){
+        let m3cortados ={
+            lunes: 0,
+            martes: 0,
+            miercoles: 0,
+            jueves: 0,
+            viernes: 0,
+            sabado: 0,
+            domingo:0
+        };
+
+        
+        if(day == 1){
+            let fechaL = moment().format("DD/MMM/YYYY");
+            fechaL = fechaL.toUpperCase();
+            let produccionL = await this.produccionRepo.find(fechaL);
+            let totalL= this._convertProd(produccionL.rows)
+            m3cortados.lunes = totalL
+        }else if(day == 2){
+            let fechaL = moment().subtract(1, "days").format("DD/MMM/YYYY");
+            fechaL = fechaL.toUpperCase();
+            let produccionL = await this.produccionRepo.find(fechaL);
+            let totalL= this._convertProd(produccionL.rows)
+            m3cortados.lunes = totalL
+
+            let fechaMa = moment().format("DD/MMM/YYYY");
+            fechaMa = fechaMa.toUpperCase();
+            let produccionMa = await this.produccionRepo.find(fechaMa);
+            let totalMa= this._convertProd(produccionMa.rows)
+            m3cortados.martes= totalMa
+        }else if(day==3){
+            let fechaL = moment().subtract(2, "days").format("DD/MMM/YYYY");
+            fechaL = fechaL.toUpperCase();
+            let produccionL = await this.produccionRepo.find(fechaL);
+            let totalL= this._convertProd(produccionL.rows)
+            m3cortados.lunes = totalL
+
+            let fechaMa = moment().subtract(1, "days").format("DD/MMM/YYYY");
+            fechaMa = fechaMa.toUpperCase();
+            let produccionMa = await this.produccionRepo.find(fechaMa);
+            let totalMa= this._convertProd(produccionMa.rows)
+            m3cortados.martes = totalMa
+
+            let fechaMi = moment().format("DD/MMM/YYYY");
+            fechaMi = fechaMi.toUpperCase();
+            let produccionMi = await this.produccionRepo.find(fechaMi);
+            let totalMi= this._convertProd(produccionMi.rows)
+            m3cortados.miercoles = totalMi
+        }else if(day==4){
+            let fechaL = moment().subtract(3, "days").format("DD/MMM/YYYY");
+            fechaL = fechaL.toUpperCase();
+            let produccionL = await this.produccionRepo.find(fechaL);
+            let totalL= this._convertProd(produccionL.rows)
+            m3cortados.lunes = totalL
+
+            let fechaMa = moment().subtract(2, "days").format("DD/MMM/YYYY");
+            fechaMa = fechaMa.toUpperCase();
+            let produccionMa = await this.produccionRepo.find(fechaMa);
+            let totalMa= this._convertProd(produccionMa.rows)
+            m3cortados.martes = totalMa
+
+            let fechaMi = moment().subtract(1, "days").format("DD/MMM/YYYY");
+            fechaMi = fechaMi.toUpperCase();
+            let produccionMi = await this.produccionRepo.find(fechaMi);
+            let totalMi= this._convertProd(produccionMi.rows)
+            m3cortados.miercoles = totalMi
+
+            let fechaJ = moment().format("DD/MMM/YYYY");
+            fechaJ = fechaJ.toUpperCase();
+            let produccionJ = await this.produccionRepo.find(fechaJ);
+            let totalJ= this._convertProd(produccionJ.rows)
+            m3cortados.jueves = totalJ
+        }else if(day == 5){
+            let fechaL = moment().subtract(4, "days").format("DD/MMM/YYYY");
+            fechaL = fechaL.toUpperCase();
+            let produccionL = await this.produccionRepo.find(fechaL);
+            let totalL= this._convertProd(produccionL.rows)
+            m3cortados.lunes = totalL
+
+            let fechaMa = moment().subtract(3, "days").format("DD/MMM/YYYY");
+            fechaMa = fechaMa.toUpperCase();
+            let produccionMa = await this.produccionRepo.find(fechaMa);
+            let totalMa= this._convertProd(produccionMa.rows)
+            m3cortados.martes = totalMa
+
+            let fechaMi = moment().subtract(2, "days").format("DD/MMM/YYYY");
+            fechaMi = fechaMi.toUpperCase();
+            let produccionMi = await this.produccionRepo.find(fechaMi);
+            let totalMi= this._convertProd(produccionMi.rows)
+            m3cortados.miercoles = totalMi
+
+            let fechaJ  = moment().subtract(1, "days").format("DD/MMM/YYYY");
+            fechaJ = fechaJ.toUpperCase();
+            let produccionJ = await this.produccionRepo.find(fechaJ);
+            let totalJ= this._convertProd(produccionJ.rows)
+            m3cortados.jueves = totalJ
+
+            let fechaV = moment().format("DD/MMM/YYYY");
+            fechaV = fechaV.toUpperCase();
+            let produccionV = await this.produccionRepo.find(fechaV);
+            let totalV= this._convertProd(produccionV.rows)
+            m3cortados.viernes = totalV
+        }else if(day== 6){
+            let fechaL = moment().subtract(5, "days").format("DD/MMM/YYYY");
+            fechaL = fechaL.toUpperCase();
+            let produccionL = await this.produccionRepo.find(fechaL);
+            let totalL= this._convertProd(produccionL.rows)
+            m3cortados.lunes = totalL
+
+            let fechaMa = moment().subtract(4, "days").format("DD/MMM/YYYY");
+            fechaMa = fechaMa.toUpperCase();
+            let produccionMa = await this.produccionRepo.find(fechaMa);
+            let totalMa= this._convertProd(produccionMa.rows)
+            m3cortados.martes = totalMa
+
+            let fechaMi = moment().subtract(3, "days").format("DD/MMM/YYYY");
+            fechaMi = fechaMi.toUpperCase();
+            let produccionMi = await this.produccionRepo.find(fechaMi);
+            let totalMi= this._convertProd(produccionMi.rows)
+            m3cortados.miercoles = totalMi
+
+            let fechaJ  = moment().subtract(2, "days").format("DD/MMM/YYYY");
+            fechaJ = fechaJ.toUpperCase();
+            let produccionJ = await this.produccionRepo.find(fechaJ);
+            let totalJ= this._convertProd(produccionJ.rows)
+            m3cortados.jueves = totalJ
+
+            let fechaV  = moment().subtract(1, "days").format("DD/MMM/YYYY");
+            fechaV = fechaV.toUpperCase();
+            let produccionV = await this.produccionRepo.find(fechaV);
+            let totalV= this._convertProd(produccionV.rows)
+            m3cortados.viernes = totalV
+
+            let fechaS = moment().format("DD/MMM/YYYY");
+            fechaS = fechaS.toUpperCase();
+            let produccionS = await this.produccionRepo.find(fechaS);
+            let totalS= this._convertProd(produccionS.rows)
+            m3cortados.sabado = totalS
+        }else if(day == 0){
+            let fechaL = moment().subtract(6, "days").format("DD/MMM/YYYY");
+            fechaL = fechaL.toUpperCase();
+            let produccionL = await this.produccionRepo.find(fechaL);
+            let totalL= this._convertProd(produccionL.rows)
+            m3cortados.lunes = totalL
+
+            let fechaMa = moment().subtract(5, "days").format("DD/MMM/YYYY");
+            fechaMa = fechaMa.toUpperCase();
+            let produccionMa = await this.produccionRepo.find(fechaMa);
+            let totalMa= this._convertProd(produccionMa.rows)
+            m3cortados.martes = totalMa
+
+            let fechaMi = moment().subtract(4, "days").format("DD/MMM/YYYY");
+            fechaMi = fechaMi.toUpperCase();
+            let produccionMi = await this.produccionRepo.find(fechaMi);
+            let totalMi= this._convertProd(produccionMi.rows)
+            m3cortados.miercoles = totalMi
+
+            let fechaJ  = moment().subtract(3, "days").format("DD/MMM/YYYY");
+            fechaJ = fechaJ.toUpperCase();
+            let produccionJ = await this.produccionRepo.find(fechaJ);
+            let totalJ= this._convertProd(produccionJ.rows)
+            m3cortados.jueves = totalJ
+
+            let fechaV  = moment().subtract(2, "days").format("DD/MMM/YYYY");
+            fechaV = fechaV.toUpperCase();
+            let produccionV = await this.produccionRepo.find(fechaV);
+            let totalV= this._convertProd(produccionV.rows)
+            m3cortados.viernes = totalV
+
+            let fechaS  = moment().subtract(1, "days").format("DD/MMM/YYYY");
+            fechaS = fechaS.toUpperCase();
+            let produccionS = await this.produccionRepo.find(fechaS);
+            let totalS= this._convertProd(produccionS.rows)
+            m3cortados.sabado = totalS
+        }
+        
+        return m3cortados
     }
 };
 
